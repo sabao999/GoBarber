98c1963e498b9ec430cd0e5324e2cc941102b6f5
sabao999 <sabao999@gmail.com>
2020-10-19 20:56:52 -0400

    frontend init
    frontend init

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import GlobalStyle from './styles/global';
+// import SignIn from './pages/SignIn/Index';
+import SignUp from './pages/SignUp/Index';
+
+const App: React.FC = () => (
+  <>
+    <SignUp />
+    <GlobalStyle />
+  </>
+);
+
+export default App;
