00d5d969239777e2d56665ea1485d923f712a412
sabao999 <sabao999@gmail.com>
2020-10-20 14:51:28 -0400

    frontend Estruturas das paginas
    Estruturas das paginas terminado

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
 import GlobalStyle from './styles/global';
-// import SignIn from './pages/SignIn/Index';
-import SignUp from './pages/SignUp/Index';
+import SignIn from './pages/SignIn/Index';
+// import SignUp from './pages/SignUp/Index';
 
 const App: React.FC = () => (
   <>
-    <SignUp />
+    <SignIn />
     <GlobalStyle />
   </>
 );
