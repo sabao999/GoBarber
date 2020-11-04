2870bfe090452e1954009463c2ae2faab3eba8ce
sabao999 <sabao999@gmail.com>
2020-11-04 19:26:17 -0400

    pasta fantasma
    pasta fantasma

--- backend/src/modules/users/providers/index.ts
+++
@@ -1,5 +0,0 @@
-import { container } from 'tsyringe';
-import IHashProvider from './HashProvider/models/IHashPorvider';
-import BCryptHashProvider from './HashProvider/implementations/BCryptHashProvider';
-
-container.registerSingleton<IHashProvider>('HashProvider', BCryptHashProvider);
